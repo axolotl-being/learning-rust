2f3e7064fa75901246622d320c03d1c527f3f44f
axolotl-being <nora.and.the.moon@gmail.com>
2023-07-01 12:12:44 -0500

    chapter 2

--- guessing_game/target/debug/deps/guessing_game-076bd6bf907fce8b.d
+++ guessing_game/target/debug/deps/guessing_game-076bd6bf907fce8b.d
@@ -1,5 +1,5 @@
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-076bd6bf907fce8b.rmeta: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-076bd6bf907fce8b.rmeta: src\main.rs
 
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-076bd6bf907fce8b.d: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-076bd6bf907fce8b.d: src\main.rs
 
 src\main.rs:
--- guessing_game/target/debug/deps/guessing_game-e1e271f609797a3c.d
+++ guessing_game/target/debug/deps/guessing_game-e1e271f609797a3c.d
@@ -1,5 +1,5 @@
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-e1e271f609797a3c.rmeta: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-e1e271f609797a3c.rmeta: src\main.rs
 
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-e1e271f609797a3c.d: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\guessing_game\target\debug\deps\guessing_game-e1e271f609797a3c.d: src\main.rs
 
 src\main.rs:
