e3618994edd644cd8cde2cac4b6efafa88fc9a24
axolotl-being <nora.and.the.moon@gmail.com>
2023-07-02 23:39:23 -0500

    prep for chapter 3 exercises

--- datatypes/target/debug/deps/datatypes-98536b9d373ff417.d
+++ datatypes/target/debug/deps/datatypes-98536b9d373ff417.d
@@ -1,5 +1,5 @@
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-98536b9d373ff417.rmeta: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-98536b9d373ff417.rmeta: src\main.rs
 
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-98536b9d373ff417.d: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-98536b9d373ff417.d: src\main.rs
 
 src\main.rs:
--- datatypes/target/debug/deps/datatypes-b9e7a996f3bce04e.d
+++ datatypes/target/debug/deps/datatypes-b9e7a996f3bce04e.d
@@ -1,5 +1,5 @@
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-b9e7a996f3bce04e.rmeta: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-b9e7a996f3bce04e.rmeta: src\main.rs
 
-c:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-b9e7a996f3bce04e.d: src\main.rs
+C:\Users\Nowora\Documents\GitHub\learning-rust\learning-rust\datatypes\target\debug\deps\datatypes-b9e7a996f3bce04e.d: src\main.rs
 
 src\main.rs:
